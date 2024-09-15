f8984b52dc4e2b35a4b35cd135282fca71bcb2e9
ziiii08 <fauzifajaralfalah56@gmail.com>
2024-09-15 10:31:33 +0700

    feat: add new images and colors

--- lib/core/helpers/assets_images.dart
+++ lib/core/helpers/assets_images.dart
@@ -2,4 +2,7 @@ class AssetsImages {
   static const String _path = 'assets/images';
 
   static const String logo = '$_path/mbooking.png';
+  static const String slider1 = '$_path/slider-1.png';
+  static const String slider2 = '$_path/slider-2.png';
+  static const String slider3 = '$_path/slider-3.png';
 }
---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color black = Color(0xFF000000);
+  static const Color yellow = Color(0xFFFCC434);
+  static const Color gray = Color(0xFF686868);
+  static const Color outline = Color(0xFFE6E6E6);
+  static const Color border = Color(0xFF262626);
+}
