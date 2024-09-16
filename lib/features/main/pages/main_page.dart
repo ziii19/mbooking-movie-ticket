4a52a2508e387af4882c4b923241f0a63509f90d
ziiii08 <fauzifajaralfalah56@gmail.com>
2024-09-16 07:14:51 +0700

    feat: create custom bottom nav and main page dummy

---
+++ lib/features/main/pages/main_page.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:mbooking/core/theme/app_colors.dart';
+
+import '../widgets/custom_bottom_nav.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int _selectedIndex = 0;
+  final pages = <Widget>[
+    const Center(
+        child: Text(
+      'home',
+      style: TextStyle(color: AppColors.white),
+    )),
+    const Center(
+        child: Text(
+      'ticket',
+      style: TextStyle(color: AppColors.white),
+    )),
+    const Center(
+        child: Text(
+      'movie',
+      style: TextStyle(color: AppColors.white),
+    )),
+    const Center(
+        child: Text(
+      'profile',
+      style: TextStyle(color: AppColors.white),
+    )),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: pages[_selectedIndex],
+      bottomNavigationBar: CustomBottomNavigationBar(
+        selectedIndex: _selectedIndex,
+        onItemTapped: (index) {
+          _selectedIndex = index;
+          setState(() {});
+        },
+        items: [
+          NavBarItem(
+            icon: Icons.home,
+            label: 'Home',
+          ),
+          NavBarItem(
+            icon: Icons.confirmation_num,
+            label: 'Ticket',
+          ),
+          NavBarItem(
+            icon: Icons.movie_outlined,
+            label: 'Movie',
+          ),
+          NavBarItem(
+            icon: Icons.person,
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/features/main/widgets/custom_bottom_nav.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+import '../../../core/theme/app_colors.dart';
+
+class CustomBottomNavigationBar extends StatelessWidget {
+  final int selectedIndex;
+  final ValueChanged<int> onItemTapped;
+  final List<NavBarItem> items;
+
+  const CustomBottomNavigationBar({
+    super.key,
+    required this.selectedIndex,
+    required this.onItemTapped,
+    required this.items,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 70,
+      decoration: const BoxDecoration(
+        border: Border(
+          top: BorderSide(
+            color: AppColors.border,
+            width: 2,
+          ),
+        ),
+        color: AppColors.black,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: items.map((item) {
+          int index = items.indexOf(item);
+          return InkWell(
+            borderRadius: BorderRadius.circular(99999),
+            onTap: () => onItemTapped(index),
+            child: SizedBox(
+              height: 70,
+              width: 70,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    item.icon,
+                    color: selectedIndex == index
+                        ? AppColors.yellow
+                        : AppColors.white,
+                    size: 25,
+                  ),
+                  const SizedBox(height: 5.0),
+                  Text(
+                    item.label,
+                    style: TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.w700,
+                      color: selectedIndex == index
+                          ? AppColors.yellow
+                          : AppColors.white,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          );
+        }).toList(),
+      ),
+    );
+  }
+}
+
+class NavBarItem {
+  final IconData icon;
+  final String label;
+
+  NavBarItem({
+    required this.icon,
+    required this.label,
+  });
+}
