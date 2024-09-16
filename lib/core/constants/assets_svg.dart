0a41929756cd9fce7b4517a4a1c5a87b3f068acf
ziiii08 <fauzifajaralfalah56@gmail.com>
2024-09-16 20:01:46 +0700

    feat: add flutter svg and assets svg

---
+++ lib/core/constants/assets_svg.dart
@@ -0,0 +1,22 @@
+class AssetsSvg {
+  static const String _path = 'assets/svg/icons';
+  static const String arrowLeft = '$_path/arrow-left.svg';
+  static const String arrowRight = '$_path/arrow-right.svg';
+  static const String calendar = '$_path/calendar.svg';
+  static const String call = '$_path/call.svg';
+  static const String clock = '$_path/clock.svg';
+  static const String edit = '$_path/edit-2.svg';
+  static const String home = '$_path/home.svg';
+  static const String location = '$_path/location.svg';
+  static const String lock = '$_path/lock.svg';
+  static const String moneySend = '$_path/money-send.svg';
+  static const String note = '$_path/note.svg';
+  static const String notification = '$_path/notification.svg';
+  static const String searchNormal = '$_path/search-normal.svg';
+  static const String seat = '$_path/seat.svg';
+  static const String shoppingCart = '$_path/shopping-cart.svg';
+  static const String ticket = '$_path/ticket.svg';
+  static const String ticket2 = '$_path/ticket-2.svg';
+  static const String user = '$_path/user.svg';
+  static const String video = '$_path/video.svg';
+}
