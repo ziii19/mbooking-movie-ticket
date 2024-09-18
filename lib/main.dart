bc80057727d233a72162292a45e8c8d99cb9c702
ziiii08 <fauzifajaralfalah56@gmail.com>
2024-09-18 12:53:06 +0700

    feat: register bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mbooking/features/main/pages/cubit/nav_index_cubit.dart';
+import 'package:mbooking/features/movie/presentation/blocs/cubit/category_index_cubit.dart';
 
 import 'core/theme/theme.dart';
+import 'core/utils/rand_int.dart';
 import 'features/auth/presentation/blocs/auth/auth_bloc.dart';
 import 'features/auth/presentation/blocs/user/user_bloc.dart';
 import 'features/auth/presentation/pages/auth/auth_page.dart';
 import 'features/main/pages/main_page.dart';
+import 'features/movie/presentation/blocs/movie/movies_bloc.dart';
+import 'features/movie/presentation/blocs/movie_detail/movie_detail_bloc.dart';
 import 'firebase_options.dart';
 import 'locator.dart';
 
@@ -25,6 +30,18 @@ void main() async {
     BlocProvider(
       create: (context) => sl<UserBloc>(),
     ),
+    BlocProvider(
+      create: (context) => sl<MoviesBloc>(),
+    ),
+    BlocProvider(
+      create: (context) => NavIndexCubit(),
+    ),
+    BlocProvider(
+      create: (context) => CategoryIndexCubit(),
+    ),
+    BlocProvider(
+      create: (context) => sl<MovieDetailBloc>(),
+    ),
   ], child: const MainApp()));
 }
 
@@ -40,6 +57,7 @@ class _MainAppState extends State<MainApp> {
   void initState() {
     super.initState();
     context.read<UserBloc>().add(GetUser());
+    context.read<MoviesBloc>().add(BlocGetMovies(page: generateRandomInt(5)));
   }
 
   @override
