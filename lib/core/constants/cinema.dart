3f675248c59c0d4a842458cae5adb430619f57d3
ziiii08 <fauzifajaralfalah56@gmail.com>
2024-09-18 15:43:59 +0700

    feat: add svg cinema and create dummy list cinema

---
+++ lib/core/constants/cinema.dart
@@ -0,0 +1,27 @@
+import 'package:mbooking/core/constants/assets_svg.dart';
+
+class Cinema {
+  final String name;
+  final String location;
+  final String icon;
+
+  Cinema({required this.name, required this.location, required this.icon});
+}
+
+List<Cinema> listCinema = [
+  Cinema(
+    name: 'Vincom Ocean Park CGV',
+    location: '4.55 km  |  Da Ton, Gia Lam, Ha Noi',
+    icon: AssetsSvg.cgv,
+  ),
+  Cinema(
+    name: 'Aeon Mall CGV',
+    location: '9.32 km  |  27 Co Linh, Long Bien, Ha Noi',
+    icon: AssetsSvg.cgv,
+  ),
+  Cinema(
+    name: 'Lotte Cinema Long Bien',
+    location: '14.3 km  |  7-9 Nguyen Van Linh, Long Bien, Ha Noi',
+    icon: AssetsSvg.lotte,
+  ),
+];
