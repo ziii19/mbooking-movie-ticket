71b13cb66cbd74d7a22d8058b66039b11d081a93
ziiii08 <fauzifajaralfalah56@gmail.com>
2024-09-18 16:32:58 +0700

    feat: add new color

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -5,6 +5,7 @@ class AppColors {
   static const Color gray1 = Color(0xFFBFBFBF);
   static const Color black = Color(0xFF000000);
   static const Color yellow = Color(0xFFFCC434);
+  static const Color yellow9 = Color(0xFF261D08);
   static const Color gray = Color(0xFF686868);
   static const Color outline = Color(0xFFE6E6E6);
   static const Color border = Color(0xFF262626);
