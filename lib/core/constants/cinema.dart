148e1996b594812b45a666b9380c38263f12a129
ziiii08 <fauzifajaralfalah56@gmail.com>
2024-09-20 10:18:03 +0700

    feat: add field price

--- lib/core/constants/cinema.dart
+++ lib/core/constants/cinema.dart
@@ -4,8 +4,9 @@ class Cinema {
   final String name;
   final String location;
   final String icon;
+  final int price;
 
-  Cinema({required this.name, required this.location, required this.icon});
+  Cinema({required this.name, required this.location, required this.icon, required this.price,});
 }
 
 List<Cinema> listCinema = [
@@ -13,15 +14,18 @@ List<Cinema> listCinema = [
     name: 'Vincom Ocean Park CGV',
     location: '4.55 km  |  Da Ton, Gia Lam, Ha Noi',
     icon: AssetsImages.cgv,
+    price: 27000,
   ),
   Cinema(
     name: 'Aeon Mall CGV',
     location: '9.32 km  |  27 Co Linh, Long Bien, Ha Noi',
     icon: AssetsImages.cgv,
+    price: 30000,
   ),
   Cinema(
     name: 'Lotte Cinema Long Bien',
     location: '14.3 km  |  7-9 Nguyen Van Linh, Long Bien, Ha Noi',
     icon: AssetsImages.lotte,
+    price: 25000,
   ),
 ];
