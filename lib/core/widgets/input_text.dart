f0d9a49328f80070bae4fd2fe490048455831f1c
ziiii08 <fauzifajaralfalah56@gmail.com>
2024-09-22 02:02:46 +0700

    feat: add new properti

--- lib/core/widgets/input_text.dart
+++ lib/core/widgets/input_text.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import '../theme/app_colors.dart';
 
@@ -7,12 +8,16 @@ class InputText extends StatelessWidget {
   final String hintText;
   final TextEditingController? controller;
   final bool validator;
+  final TextInputType? keyboardType;
+  final List<TextInputFormatter>? inputFormatters;
   const InputText({
     super.key,
     required this.label,
     required this.hintText,
     this.controller,
     this.validator = true,
+    this.keyboardType,
+    this.inputFormatters,
   });
 
   @override
@@ -20,6 +25,8 @@ class InputText extends StatelessWidget {
     return TextFormField(
       style: const TextStyle(color: AppColors.white),
       controller: controller,
+      keyboardType: keyboardType,
+      inputFormatters: inputFormatters,
       validator: validator
           ? (value) {
               if (value == null || value.isEmpty) {
