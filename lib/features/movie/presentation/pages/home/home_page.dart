2313562a30b4aae38c1761c044bf6bac90bfd597
ziiii08 <fauzifajaralfalah56@gmail.com>
2024-09-22 08:00:42 +0700

    feat: add search movie

--- lib/features/movie/presentation/pages/home/home_page.dart
+++ lib/features/movie/presentation/pages/home/home_page.dart
@@ -12,6 +12,7 @@ import 'package:mbooking/features/movie/presentation/pages/movie_detail_booking/
 import '../../../../../core/utils/get_genre_names.dart';
 import '../../../../auth/presentation/blocs/user/user_bloc.dart';
 import '../../../../booking/presentation/cubit/booking_cubit.dart';
+import '../../../domain/entities/movie.dart';
 import '../../blocs/movie/movies_bloc.dart';
 import '../../widgets/movie_slider_card.dart';
 import '../../widgets/title_section.dart';
@@ -32,47 +33,126 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<Movie> movies = [];
+  List<Movie> filteredMovies = [];
+  String searchQuery = '';
+
+  void _filterMovies(String query) {
+    final filtered = movies.where((movie) {
+      final nameLower = movie.title.toLowerCase();
+      final queryLower = query.toLowerCase();
+      return nameLower.contains(queryLower);
+    }).toList();
+    setState(() {
+      searchQuery = query;
+      filteredMovies = filtered;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-          child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 10),
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              const _UserSection(),
-              const SizedBox(height: 16),
-              TextFormField(
-                decoration: const InputDecoration(
-                  contentPadding: EdgeInsets.all(12),
-                  prefixIcon: Icon(Icons.search, color: AppColors.gray),
-                  hintText: 'Search',
-                  fillColor: AppColors.enabled,
-                  filled: true,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(8),
+      body: BlocBuilder<MoviesBloc, MoviesState>(
+        builder: (context, state) {
+          if (state is MoviesSuccess) {
+            movies = [...state.nowPlayingMovies, ...state.upcomingMovies];
+            if (filteredMovies.isEmpty && searchQuery.isEmpty) {
+              filteredMovies = movies;
+            }
+          }
+
+          return SafeArea(
+              child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 10),
+            child: SingleChildScrollView(
+              child: Column(children: [
+                const _UserSection(),
+                const SizedBox(height: 16),
+                TextFormField(
+                  onChanged: _filterMovies,
+                  style: const TextStyle(color: AppColors.white),
+                  decoration: const InputDecoration(
+                    contentPadding: EdgeInsets.all(12),
+                    prefixIcon: Icon(Icons.search, color: AppColors.gray),
+                    hintText: 'Search',
+                    fillColor: AppColors.enabled,
+                    filled: true,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(8),
+                      ),
+                      borderSide: BorderSide.none,
                     ),
-                    borderSide: BorderSide.none,
                   ),
                 ),
-              ),
-              const SizedBox(height: 16),
-              const _NowPlayingSlider(),
-              const SizedBox(height: 16),
-              const _ComingSoonSection(),
-              const SizedBox(height: 16),
-              const _PromoSection(),
-              const SizedBox(height: 16),
-              const _ServiceSection(),
-              const SizedBox(height: 16),
-              const _MovieNewsSection(),
-              const SizedBox(height: 20),
-            ],
-          ),
-        ),
-      )),
+                const SizedBox(height: 16),
+                if (searchQuery.isNotEmpty) ...[
+                  if (filteredMovies.isEmpty) ...[
+                    const Center(
+                      child: Text(
+                        'No movies found',
+                        style: TextStyle(color: AppColors.white),
+                      ),
+                    )
+                  ] else ...[
+                    Container(
+                      height: ((filteredMovies.length / 2).ceil() * 300) + 150,
+                      padding: const EdgeInsets.symmetric(vertical: 20),
+                      child: GridView.count(
+                        crossAxisCount: 2,
+                        shrinkWrap: true,
+                        physics: const NeverScrollableScrollPhysics(),
+                        childAspectRatio: 2 / 3,
+                        children: filteredMovies
+                            .map((movie) => SizedBox(
+                                  width: 170,
+                                  child: Column(
+                                    children: [
+                                      ClipRRect(
+                                        borderRadius: BorderRadius.circular(10),
+                                        child: Image.network(
+                                          'https://image.tmdb.org/t/p/w500${movie.posterPath}',
+                                          width: 170,
+                                          height: 240,
+                                          fit: BoxFit.cover,
+                                        ),
+                                      ),
+                                      const SizedBox(height: 10),
+                                      SizedBox(
+                                        width: 165,
+                                        child: Text(
+                                          movie.title,
+                                          style: const TextStyle(
+                                            fontWeight: FontWeight.w500,
+                                            fontSize: 16,
+                                            color: AppColors.white,
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ))
+                            .toList(),
+                      ),
+                    ),
+                  ]
+                ] else ...[
+                  const _NowPlayingSlider(),
+                  const SizedBox(height: 16),
+                  const _ComingSoonSection(),
+                  const SizedBox(height: 16),
+                  const _PromoSection(),
+                  const SizedBox(height: 16),
+                  const _ServiceSection(),
+                  const SizedBox(height: 16),
+                  const _MovieNewsSection(),
+                  const SizedBox(height: 20),
+                ],
+              ]),
+            ),
+          ));
+        },
+      ),
     );
   }
 }
