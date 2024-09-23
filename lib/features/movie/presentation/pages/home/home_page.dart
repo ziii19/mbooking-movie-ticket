22d9fd5be3fe1584b9053e2bb18c4465b9670446
ziiii08 <fauzifajaralfalah56@gmail.com>
2024-09-23 09:54:59 +0700

    feat: add refresh indicator and fix search layout

--- lib/features/movie/presentation/pages/home/home_page.dart
+++ lib/features/movie/presentation/pages/home/home_page.dart
@@ -10,6 +10,7 @@ import 'package:mbooking/features/movie/presentation/blocs/cubit/category_index_
 import 'package:mbooking/features/movie/presentation/pages/movie_detail_booking/movie_detail_booking.dart';
 
 import '../../../../../core/utils/get_genre_names.dart';
+import '../../../../../core/utils/rand_int.dart';
 import '../../../../auth/presentation/blocs/user/user_bloc.dart';
 import '../../../../booking/presentation/cubit/booking_cubit.dart';
 import '../../../domain/entities/movie.dart';
@@ -52,106 +53,135 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: BlocBuilder<MoviesBloc, MoviesState>(
-        builder: (context, state) {
-          if (state is MoviesSuccess) {
-            movies = [...state.nowPlayingMovies, ...state.upcomingMovies];
-            if (filteredMovies.isEmpty && searchQuery.isEmpty) {
-              filteredMovies = movies;
+      body: RefreshIndicator(
+        onRefresh: () async {
+          context
+              .read<MoviesBloc>()
+              .add(BlocGetMovies(page: generateRandomInt(5)));
+        },
+        child: BlocBuilder<MoviesBloc, MoviesState>(
+          builder: (context, state) {
+            if (state is MoviesSuccess) {
+              movies = [...state.nowPlayingMovies, ...state.upcomingMovies];
+              if (filteredMovies.isEmpty && searchQuery.isEmpty) {
+                filteredMovies = movies;
+              }
             }
-          }
 
-          return SafeArea(
-              child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 10),
-            child: SingleChildScrollView(
-              child: Column(children: [
-                const _UserSection(),
-                const SizedBox(height: 16),
-                TextFormField(
-                  onChanged: _filterMovies,
-                  style: const TextStyle(color: AppColors.white),
-                  decoration: const InputDecoration(
-                    contentPadding: EdgeInsets.all(12),
-                    prefixIcon: Icon(Icons.search, color: AppColors.gray),
-                    hintText: 'Search',
-                    fillColor: AppColors.enabled,
-                    filled: true,
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(8),
+            return SafeArea(
+                child: Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 16.0, vertical: 10),
+              child: SingleChildScrollView(
+                child: Column(children: [
+                  const _UserSection(),
+                  const SizedBox(height: 16),
+                  TextFormField(
+                    onChanged: _filterMovies,
+                    style: const TextStyle(color: AppColors.white),
+                    decoration: const InputDecoration(
+                      contentPadding: EdgeInsets.all(12),
+                      prefixIcon: Icon(Icons.search, color: AppColors.gray),
+                      hintText: 'Search',
+                      fillColor: AppColors.enabled,
+                      filled: true,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(8),
+                        ),
+                        borderSide: BorderSide.none,
                       ),
-                      borderSide: BorderSide.none,
                     ),
                   ),
-                ),
-                const SizedBox(height: 16),
-                if (searchQuery.isNotEmpty) ...[
-                  if (filteredMovies.isEmpty) ...[
-                    const Center(
-                      child: Text(
-                        'No movies found',
-                        style: TextStyle(color: AppColors.white),
-                      ),
-                    )
-                  ] else ...[
-                    Container(
-                      height: ((filteredMovies.length / 2).ceil() * 300) + 150,
-                      padding: const EdgeInsets.symmetric(vertical: 20),
-                      child: GridView.count(
-                        crossAxisCount: 2,
-                        shrinkWrap: true,
-                        physics: const NeverScrollableScrollPhysics(),
-                        childAspectRatio: 2 / 3,
-                        children: filteredMovies
-                            .map((movie) => SizedBox(
-                                  width: 170,
-                                  child: Column(
-                                    children: [
-                                      ClipRRect(
-                                        borderRadius: BorderRadius.circular(10),
-                                        child: Image.network(
-                                          'https://image.tmdb.org/t/p/w500${movie.posterPath}',
+                  const SizedBox(height: 16),
+                  if (searchQuery.isNotEmpty) ...[
+                    if (filteredMovies.isEmpty) ...[
+                      const Center(
+                        child: Text(
+                          'No movies found',
+                          style: TextStyle(color: AppColors.white),
+                        ),
+                      )
+                    ] else ...[
+                      Container(
+                        height:
+                            ((filteredMovies.length / 2).ceil() * 300) + 150,
+                        padding: const EdgeInsets.symmetric(vertical: 20),
+                        child: LayoutBuilder(
+                          builder: (context, constraints) {
+                            final width = constraints.maxWidth;
+                            final crossAxisCount = width > 600 ? 3 : 2;
+                            final aspectRatio = width / crossAxisCount / 320;
+                            return GridView.count(
+                              crossAxisCount: crossAxisCount,
+                              shrinkWrap: true,
+                              physics: const NeverScrollableScrollPhysics(),
+                              childAspectRatio: aspectRatio,
+                              crossAxisSpacing: 16,
+                              children: filteredMovies
+                                  .map((movie) => GestureDetector(
+                                        onTap: () {
+                                          Navigator.push(
+                                            context,
+                                            MovieDetailBooking.route(movie.id),
+                                          );
+                                        },
+                                        child: SizedBox(
                                           width: 170,
-                                          height: 240,
-                                          fit: BoxFit.cover,
-                                        ),
-                                      ),
-                                      const SizedBox(height: 10),
-                                      SizedBox(
-                                        width: 165,
-                                        child: Text(
-                                          movie.title,
-                                          style: const TextStyle(
-                                            fontWeight: FontWeight.w500,
-                                            fontSize: 16,
-                                            color: AppColors.white,
+                                          child: Column(
+                                            children: [
+                                              ClipRRect(
+                                                borderRadius:
+                                                    BorderRadius.circular(10),
+                                                child: Image.network(
+                                                  'https://image.tmdb.org/t/p/w500${movie.posterPath}',
+                                                  width: 170,
+                                                  height: 240,
+                                                  fit: BoxFit.cover,
+                                                ),
+                                              ),
+                                              const SizedBox(height: 10),
+                                              SizedBox(
+                                                width: 165,
+                                                child: Text(
+                                                  movie.title,
+                                                  maxLines: 2,
+                                                  overflow:
+                                                      TextOverflow.ellipsis,
+                                                  style: const TextStyle(
+                                                    fontWeight: FontWeight.w500,
+                                                    fontSize: 16,
+                                                    color: AppColors.white,
+                                                  ),
+                                                ),
+                                              ),
+                                            ],
                                           ),
                                         ),
-                                      ),
-                                    ],
-                                  ),
-                                ))
-                            .toList(),
+                                      ))
+                                  .toList(),
+                            );
+                          },
+                        ),
                       ),
-                    ),
-                  ]
-                ] else ...[
-                  const _NowPlayingSlider(),
-                  const SizedBox(height: 16),
-                  const _ComingSoonSection(),
-                  const SizedBox(height: 16),
-                  const _PromoSection(),
-                  const SizedBox(height: 16),
-                  const _ServiceSection(),
-                  const SizedBox(height: 16),
-                  const _MovieNewsSection(),
-                  const SizedBox(height: 20),
-                ],
-              ]),
-            ),
-          ));
-        },
+                    ]
+                  ] else ...[
+                    const _NowPlayingSlider(),
+                    const SizedBox(height: 16),
+                    const _ComingSoonSection(),
+                    const SizedBox(height: 16),
+                    const _PromoSection(),
+                    const SizedBox(height: 16),
+                    const _ServiceSection(),
+                    const SizedBox(height: 16),
+                    const _MovieNewsSection(),
+                    const SizedBox(height: 20),
+                  ],
+                ]),
+              ),
+            ));
+          },
+        ),
       ),
     );
   }
